037d34d0854b7db4c991439415ce80e261a45ee9
Yepod <Williamrossander@hotmail.com>
2026-01-16 13:59:55 +0100

    permit all test

--- src/main/java/se/jensen/william/springboot/config/SecurityConfig.java
+++ src/main/java/se/jensen/william/springboot/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html"
                         ).permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
---
+++ src/main/java/se/jensen/william/springboot/entities/Friendship.java
@@ -0,0 +1,5 @@
+package se.jensen.william.springboot.entities;
+
+public class Friendship {
+
+}
